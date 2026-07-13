ac6b79acf998b2bd51a9c4c2d0e94ca4d1672962
Satya Mythri Reejinthala <satyamythrir06@gmail.com>
2026-07-13 13:45:06 +0530

    file

---
+++ app.py
@@ -0,0 +1,47 @@
+
+import streamlit as st
+from google import genai
+
+# Replace with your Gemini API Key
+client = genai.Client(api_key="GEMINI_API_KEY")
+st.set_page_config(page_title="AI Learning Buddy", page_icon="🎓")
+
+st.title("🎓 AI Learning Buddy")
+
+topic = st.text_input("Enter a Topic")
+
+option = st.selectbox(
+    "Choose Activity",
+    [
+        "Explain Concept",
+        "Real-Life Example",
+        "Generate Quiz"
+    ]
+)
+
+if st.button("Generate"):
+
+    if topic.strip() == "":
+        st.warning("Please enter a topic.")
+    else:
+
+        if option == "Explain Concept":
+            prompt = f"Explain {topic} in simple language for a beginner."
+
+        elif option == "Real-Life Example":
+            prompt = f"Give one simple real-life example of {topic}."
+
+        elif option == "Generate Quiz":
+            prompt = f"Create 5 multiple choice questions on {topic} with answers."
+
+        try:
+            response = client.models.generate_content(
+                model="gemini-flash-latest",
+                contents=prompt
+            )
+
+            st.subheader("Result")
+            st.write(response.text)
+
+        except Exception as e:
+            st.error(f"Error: {e}")
