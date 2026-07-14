3aac9fdc18c68442b86279832c949c42a691b167
Satya Mythri Reejinthala <satyamythrir06@gmail.com>
2026-07-14 11:33:53 +0530

    Update model in content generation to TTS version

--- app.py
+++ app.py
@@ -40,7 +40,7 @@ if st.button("Generate"):
 
         try:
             response = client.models.generate_content(
-                model="gemini-2.5-flash",
+                model="gemini-2.5-flash-TTS",
                 contents=prompt
             )
 
